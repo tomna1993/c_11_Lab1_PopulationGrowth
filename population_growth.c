cf3c8269904774bddf962552d22521edece471b8
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-27 17:36:40 +0200

    Help user with incorrect inputs

--- population_growth.c
+++ population_growth.c
@@ -14,19 +14,31 @@ int main(void)
 	do
 	{
 		startSize = get_int("Start size: ");
+
+		if(startSize < 9)
+		{
+			printf("Your llamas population will stagnate under 9 llamas. Buy more!\n");
+		}
 	} while (startSize < 9);
 	
 	do
 	{
 		endSize = get_int("End size: ");	
+
+		if(endSize <= startSize)
+		{
+			printf("You already reach the given llamas population. Give another number!\n");
+		}
 	} while (endSize <= startSize);
 	
+
 	// Calculate growth in years
 	int yearsOfGrowth = 0;
 	
 	yearsOfGrowth = calculateGrowthInYears(startSize, endSize);
+	
+	
 	// Output result
-
 	printf("Years: %i", yearsOfGrowth);
 }
 
@@ -35,7 +47,7 @@ int calculateGrowthInYears(int startNum, int endNum)
 	int n = 0;
 	do
 	{
-		startNum = startNum + startNum / 3 - startNum / 4;
+		startNum = startNum + (startNum / 3) - (startNum / 4);
 
 		n++;
 
