5a1298d6b25a3f7fe48111f0e882554cf15e6124
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-27 15:53:16 +0200

    First implementation

--- population_growth.c
+++ population_growth.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <cs50.c>
 
 int main(void)
 {
 	/* Ask user input for
 	Start size: 
 	End size: */
+	int startSize = get_int("Start size: ");
+	int endSize = get_int("End size: ");
 
 	// Calculate growth in years
+	int yearsOfGrowth = 0;
+	do
+	{
+		startSize = startSize + startSize / 3 - startSize / 4;
+
+		yearsOfGrowth++;
+
+	} while (endSize > startSize);
+
 
 	// Output result
+
+	printf("Years: %i", yearsOfGrowth);
 }
